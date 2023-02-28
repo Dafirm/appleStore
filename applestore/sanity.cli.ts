77afd69552dacecb78dce7d371951959c21dbb67
dafirm <ayanfeoyeniyi@gmail.com>
2023-02-28 09:32:37 +0200

    header

---
+++ applestore/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ei0gphsu',
+    dataset: 'production'
+  }
+})
---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <Image
+          src="https://rb.gy/vsvv2o"
+          fill
+          
+        alt='logo'
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header
